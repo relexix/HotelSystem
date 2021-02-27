0fc81f0dceea748541ee99b15742a6b0fbea4aa8
relexix <alex.boitor90@gmail.com>
2021-02-27 18:27:49 +0200

    NEW WORK 2

--- src/main/java/hotelsystem/Main.java
+++ src/main/java/hotelsystem/Main.java
@@ -3,13 +3,10 @@ package hotelsystem;
 import dbconnection.DataBaseUtils;
 import dbconnection.HibernateUtils;
 import hotelsystem.tables.Booking;
-import hotelsystem.tables.Client;
-import hotelsystem.tables.Room;
 import repository.BookingRepository;
-import repository.ClientRepository;
-import repository.RoomRepository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -22,23 +19,25 @@ public class Main {
         DataBaseUtils.showTables();
         DataBaseUtils.getConnection();
 
-        //CREATE BOOKING
-        Booking booking1 = new Booking(1, 22, 28, "cash", 200);
+//        //CREATE BOOKING
+//        Booking booking1 = new Booking(1, 22, 28, "cash", 200);
         BookingRepository bookingRepository = new BookingRepository();
-        bookingRepository.addBooking(booking1);
-
-        //Create Client
-        Client client1 = new Client(1, "Alex", "Boitor", 30, "Baia Mare",
-                555662552, "alex.boitor90@gmail.com");
-        ClientRepository clientRepository = new ClientRepository();
-        clientRepository.addClient(client1);
-
-        //Create Room
-        Room room1 = new Room(1, 1, "small", 1, 200);
-        RoomRepository roomRepository = new RoomRepository();
-        roomRepository.addRoom(room1);
+//        bookingRepository.addBooking(booking1);
+//
+//        //Create Client
+//        Client client1 = new Client(1, "Alex", "Boitor", 30, "Baia Mare",
+//                555662552, "alex.boitor90@gmail.com");
+//        ClientRepository clientRepository = new ClientRepository();
+//        clientRepository.addClient(client1);
+//
+//        //Create Room
+//        Room room1 = new Room(1, 1, "small", 1, 200);
+//        RoomRepository roomRepository = new RoomRepository();
+//        roomRepository.addRoom(room1);
 
         //SHOW BOOKING LIST
+        BookingRepository.showBookingList();
+
 
 
     }
--- src/main/java/repository/BookingRepository.java
+++ src/main/java/repository/BookingRepository.java
@@ -5,6 +5,8 @@ import hotelsystem.tables.Booking;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class BookingRepository {
 
     public void addBooking(Booking booking) {
@@ -20,5 +22,68 @@ public class BookingRepository {
         transaction.commit();
 
         session.close();
+    }
+
+    public void updateBooking(Booking booking) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        System.out.println("Hibernate session started for updateBooking: ");
+
+        if (bookingExists(booking)) {
+            Transaction transaction = session.beginTransaction();
+
+            // actual save into DB
+            session.update(booking);
+
+            // end transaction
+            transaction.commit();
+
+            session.close();
+        } else {
+            System.out.println("Booking was not found:" + booking);
+        }
+    }
+
+    public boolean bookingExists(Booking booking) {
+        return findById(booking.getBookingId()) != null;
+    }
+
+    public Booking findById(int id) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        System.out.println("Hibernate session started for findById()");
+
+        Booking booking = session.find(Booking.class, id);
+
+        session.close();
+
+        System.out.println("Return " + booking);
+        return booking;
+    }
+
+    public void deleteBooking(Booking booking) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        System.out.println("Hibernate session started for deleteBooking: ");
+
+        if (bookingExists(booking)) {
+            Transaction transaction = session.beginTransaction();
+
+            // actual save into DB
+            session.delete(booking);
+
+            // end transaction
+            transaction.commit();
+
+            session.close();
+        } else {
+            System.out.println("Booking was not found:" + booking);
+        }
+    }
+
+    public static void showBookingList() {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        List allBooking = session.createSQLQuery("SELECT * FROM booking").list();
+        System.out.println("TABLES: ");
+        System.out.println(allBooking);
+        System.out.println("TABLES SIZE: ");
+        System.out.println(allBooking.size());
     }
 }
